1bca737d3baf4e07dd8453c236491e95e537fff3
sbondai <sbondai9@gmail.com>
2023-10-17 11:47:40 +0200

    fixed port issues

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Only for development; for production, configure allowed origins.
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
